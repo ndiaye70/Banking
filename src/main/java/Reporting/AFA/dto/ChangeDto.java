c1a268e2930295d5285620e0e1ba5bdc3256c67a
ndiaye70 <ndiaye.pape-abdoulaye@ugb.edu.sn>
2024-03-29 04:18:31 +0100

    commit Change

--- src/main/java/Reporting/AFA/dto/ChangeDto.java
+++ src/main/java/Reporting/AFA/dto/ChangeDto.java
@@ -12,19 +12,38 @@ public class ChangeDto {
 
     private String prenom;
     private String nom;
-    private String service;
+    private Change.Service service;
     private String deviseRecu;
     private String deviseRemis;
     private double montantRecu;
+    private Change.Type_Piece_Identite typePieceIdentite;
+    private String numero_piece_identité;
+    private String date_delivrance;
+    private String Telephone;
+    private String resident;
+    private Integer Passeport ;
+    private Integer Billet_Avion;
+    private Integer CNI;
+
 
     public Change toEntity() {
         Change change = new Change();
         change.setPrenom(this.prenom);
         change.setNom(this.nom);
-        change.setService(Change.Service.valueOf(this.service));
+        change.setService(this.service);
         change.setDeviseRecu(Change.Devise.valueOf(this.deviseRecu));
         change.setDeviseRemis(Change.Devise.valueOf(this.deviseRemis));
         change.setMontantRecu(this.montantRecu);
+        change.setTypePieceIdentite(this.typePieceIdentite);
+        change.setNumero_piece_identité(this.numero_piece_identité);
+        change.setDate_delivrance(this.date_delivrance);
+        change.setTelephone(this.Telephone);
+        change.setResident(Change.Resident.valueOf(this.resident));
+        change.setPasseport(this.Passeport);
+        change.setBillet_Avion(this.Billet_Avion);
+        change.setCNI(this.CNI);
         return change;
     }
+
+
 }
